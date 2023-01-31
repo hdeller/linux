34c8669eddd20cb2dba741cc3dd2af96d1bd2510
Al Viro <viro@zeniv.linux.org.uk>
2023-01-30 23:53:52 -0500

    parisc: fix livelock in uaccess
    parisc equivalent of 26178ec11ef3 "x86: mm: consolidate VM_FAULT_RETRY handling"
    If e.g. get_user() triggers a page fault and a fatal signal is caught, we might
    end up with handle_mm_fault() returning VM_FAULT_RETRY and not doing anything
    to page tables.  In such case we must *not* return to the faulting insn -
    that would repeat the entire thing without making any progress; what we need
    instead is to treat that as failed (user) memory access.
    
    Tested-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/parisc/mm/fault.c
+++ arch/parisc/mm/fault.c
@@ -455,8 +455,13 @@ retry:
 
 	fault = handle_mm_fault(vma, address, flags, regs);
 
-	if (fault_signal_pending(fault, regs))
+	if (fault_signal_pending(fault, regs)) {
+		if (!user_mode(regs)) {
+			msg = "Page fault: fault signal on kernel memory";
+			goto no_context;
+		}
 		return;
+	}
 
 	/* The fault is fully completed (including releasing mmap lock) */
 	if (fault & VM_FAULT_COMPLETED)
